6974252497a5e2e7571c2cdc14fea004e6e49fb5
Renan Barbosa <renan.barbosa@netbiis.local>
2023-03-24 15:49:06 -0300

    initial configurations

---
+++ desafio-city-event-do0/src/main/java/com/renanbarhbosa/desafiocityeventdo0/DesafioCityEventDo0Application.java
@@ -0,0 +1,13 @@
+package com.renanbarhbosa.desafiocityeventdo0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCityEventDo0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCityEventDo0Application.class, args);
+	}
+
+}
---
+++ desafio-city-event-do0/src/test/java/com/renanbarhbosa/desafiocityeventdo0/DesafioCityEventDo0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanbarhbosa.desafiocityeventdo0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCityEventDo0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
