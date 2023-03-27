956035d084dd3eb33980cc8df147981de31da1c4
Renan Barbosa <renan.barbosa@netbiis.local>
2023-03-27 12:28:42 -0300

    Services ok

--- desafio-city-event-do0/src/main/java/com/renanbarhbosa/desafiocityeventdo0/services/CityService.java
+++ desafio-city-event-do0/src/main/java/com/renanbarhbosa/desafiocityeventdo0/services/CityService.java
@@ -3,10 +3,12 @@ package com.renanbarhbosa.desafiocityeventdo0.services;
 import com.renanbarhbosa.desafiocityeventdo0.dtos.CityDTO;
 import com.renanbarhbosa.desafiocityeventdo0.entities.City;
 import com.renanbarhbosa.desafiocityeventdo0.repositories.CityRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,13 +26,40 @@ public class CityService {
         return cities.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public CityDTO findById(Long id) {
         Optional<City> obj = repository.findById(id);
         City entity = obj.orElseThrow(() -> new IllegalArgumentException("Id not found."));
         return new CityDTO(entity);
     }
 
+    @Transactional
     public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
 
+    @Transactional(readOnly = true)
+    public CityDTO update(Long id, CityDTO dto) {
+        try {
+            City entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CityDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Entity not found.");
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("Id not found." + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Integrity violation");
+        }
     }
 }
--- desafio-city-event-do0/src/main/java/com/renanbarhbosa/desafiocityeventdo0/services/EventService.java
+++ desafio-city-event-do0/src/main/java/com/renanbarhbosa/desafiocityeventdo0/services/EventService.java
@@ -1,8 +1,18 @@
 package com.renanbarhbosa.desafiocityeventdo0.services;
 
+import com.renanbarhbosa.desafiocityeventdo0.dtos.EventDTO;
+import com.renanbarhbosa.desafiocityeventdo0.entities.Event;
 import com.renanbarhbosa.desafiocityeventdo0.repositories.EventRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
@@ -10,5 +20,50 @@ public class EventService {
     @Autowired
     private EventRepository repository;
 
+    @Transactional(readOnly = true)
+    public List<EventDTO> findAll() {
+        List<Event> list = repository.findAll();
+        return list.stream().map(x -> new EventDTO(x)).collect(Collectors.toList());
+    }
 
+    @Transactional(readOnly = true)
+    public EventDTO findById(Long id) {
+        Optional<Event> obj = repository.findById(id);
+        Event entity = obj.orElseThrow(() -> new RuntimeException("Entity not found."));
+        return new EventDTO(entity);
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity = repository.save(entity);
+        return new EventDTO(entity);
+    }
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity.setDate(dto.getDate());
+            entity.setUrl(dto.getUrl());
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new RuntimeException("Id not found." + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("Id not found." + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Integrity violation.");
+        }
+    }
 }
